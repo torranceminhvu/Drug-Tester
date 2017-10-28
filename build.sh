56d29e1615fdcebd6405e0279137ab646d884e53
Minh Vu <torranceminhvu@yahoo.com>
2017-10-27 17:56:28 -0700

    Adding build script to copy files over to website and fixing problem where we don't also skip hideList

---
+++ build.sh
@@ -0,0 +1,6 @@
+echo Copying css files over
+cp -r css ../Github\ Website/torranceminhvu.github.io/
+echo Copying js files over
+cp -r js ../Github\ Website/torranceminhvu.github.io/
+echo Copying index.html over
+cp index.html ../Github\ Website/torranceminhvu.github.io/
